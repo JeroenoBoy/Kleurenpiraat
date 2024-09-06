cfdc04930f3724c77e5e9ba453e709f78d986744
Jeroen van de Geest <jeroen@vandegeest.eu>
2024-09-06 09:53:03 +0200

    Add simple webserver

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+const express = require("express")
 
-console.log("Hello World!");
+const port = 3000;
+const app = express()
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World!");
+})
+
+app.listen(port, () => {
+    console.log(`🌈 Kleurenpiraat is actief op poort ${port} 🚀`)
+})
