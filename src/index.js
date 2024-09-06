60bde87745cc7f4368bd57cf7b9914df5d60ca90
Jeroen van de Geest <jeroen@vandegeest.eu>
2024-09-06 09:59:54 +0200

    Add static file routing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require("express")
 const port = 3000;
 const app = express()
 
-app.get("/", (req, res) => {
+app.use(express.static("public"));
+
+app.get("/api", (req, res) => {
     res.status(200).send("Hello World!");
 })
 
