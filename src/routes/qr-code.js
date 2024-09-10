5431f5781da8e76d20bece96de369865f5df9254
Jeroen van de Geest <jeroen@vandegeest.eu>
2024-09-10 13:48:02 +0200

    refactor qr-code route

---
+++ src/routes/qr-code.js
@@ -0,0 +1,37 @@
+const WSConnection = require("../WSConnection")
+const qrcode = require("qrcode")
+
+const router = require("express").Router()
+const wsServer = require("..").wsServer
+
+router.get("/qr-code", (req, res) => {
+    console.log(req.baseUrl)
+    if (req.session.authenticated) {
+        res.status(403).send("You are not logged in");
+        return
+    }
+
+    const id = req.session.user.id
+    qrcode.toBuffer(req.baseUrl)
+})
+
+router.post("/qr-code", (req, res) => {
+    if (!req.session.authenticated) {
+        res.status(403).send("You are not logged in");
+        return
+    }
+    const scannedqrcode = req.body.code
+    const myId = req.session.user.id
+
+    const connection = wsServer.connections.find(it => it.id == scannedqrcode)
+    if (connection == null) {
+        res.status(400).send("User not found!")
+        return
+    }
+
+    connection.send("qr-code-scanned", `${myId}`)
+
+    res.status(200).send("Succes!")
+})
+
+module.exports = router
