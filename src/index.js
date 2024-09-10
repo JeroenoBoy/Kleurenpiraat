cc7870173e720fe416ee3a69a73b261db0392b8c
Jeroen van de Geest <jeroen@vandegeest.eu>
2024-09-10 11:49:02 +0200

    Export wsServer

--- src/index.js
+++ src/index.js
@@ -9,16 +9,17 @@ const app = express()
 
 app.use(express.static("public"));
 app.use(session({
-    secret: process.env.SECRET ?? "ready your dingus", 
-    resave: false, 
+    secret: process.env.SECRET ?? "ready your dingus",
+    resave: false,
     saveUninitialized: false,
-    cookie: {maxage: 3000000}
+    cookie: { maxage: 3000000 }
 }));
 
 const colourData = ["red", "blue", "yellow", "green"]
-function GetRandomColour() { 
-    return colours[Math.floor(Math.random()*colours.length)];
+function GetRandomColour() {
+    return colours[Math.floor(Math.random() * colours.length)];
 }
+
 
 app.get("/api", (req, res) => {
     res.status(200).send("Hello World!");
@@ -32,27 +33,27 @@ app.post("/give-cookie", (req, res) => {
     try {
         if (req.body.username == null) {
             return
-        } 
+        }
         let checkForUser = users.find((data) => req.body.username === users.name);
-        if (req.session.authenticated) { 
+        if (req.session.authenticated) {
             res.status(403).send("User already has an account");
-        } else if (!checkForUser) { 
+        } else if (!checkForUser) {
 
             //creates new user
-            let newUser = { 
+            let newUser = {
                 name: req.body.username,
                 colour: GetRandomColour()
             }
             users.push(newUser);
 
             //pushes cookie to end-user
-            req.session.authenticated = true; 
+            req.session.authenticated = true;
             req.session.user = newUser;
             res.status(200).send("Success");
         } else {
             res.status(403).send("This username already exists!");
         }
-    } catch(e) { 
+    } catch (e) {
         console.log(e);
     }
 });
@@ -61,4 +62,4 @@ const server = app.listen(port, () => {
     console.log(`🌈 Kleurenpiraat is actief op poort ${port} 🚀`)
 })
 
-createWSServer(server)
+module.exports.wsServer = createWSServer(server)
