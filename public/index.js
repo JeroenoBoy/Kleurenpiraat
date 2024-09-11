9fbbb32d93970016848c72e8f025ee46dc2a6349
Daan Meijneken <gamingkiwi22@gmail.com>
2024-09-11 12:42:43 +0200

    removed "high" contrast color

--- public/index.js
+++ public/index.js
@@ -22,7 +22,6 @@ window.onload = () => {
         element.style["background-color"] = colormap[msg]
 
         let text = document.getElementById("kleur-text")
-        text.style.color = invertColor(colormap[msg])
         text.innerHTML = colornaam[msg]
     })
 
@@ -35,23 +34,5 @@ window.onload = () => {
         console.log(`Opened socket to`, e.target.url)
         connection.send("hello");
     })
-
-    //inverts color, courtesy of StackOverflow
-    function invertColor(hexCode) {
-        if (hexCode.indexOf('#') === 0) {
-            hexCode = hexCode.slice(1);
-        }
-
-        let r = (255 - parseInt(hexCode.slice(0, 2), 16)).toString(16),
-            g = (255 - parseInt(hexCode.slice(2, 4), 16)).toString(16),
-            b = (255 - parseInt(hexCode.slice(4, 6), 16)).toString(16);
-        return '#' + padIt(r) + padIt(g) + padIt(b);
-    }
-
-    function padIt(str, len) {
-        len = len || 2;
-        let zeros = new Array(len).join('0');
-        return (zeros + str).slice(-len);
-    }
 }
 
