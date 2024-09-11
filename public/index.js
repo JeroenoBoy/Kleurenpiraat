6cd2705dd5ebc9adf1ba96912c248ce51beb1fcc
Jeroen van de Geest <jeroen@vandegeest.eu>
2024-09-11 10:49:21 +0200

    yes

--- public/index.js
+++ public/index.js
@@ -10,19 +10,23 @@ window.onload = () => {
         "green": "#55cc55",
     }
 
-    const colornaam = { 
+    const colornaam = {
         "red": "rood",
-        "yellow": "geel", 
+        "yellow": "geel",
         "blue": "blauw",
         "green": "groen"
     }
 
-    connection.on("color-change", (msg) => {
+    connection.on("color-change", msg => {
         const element = document.querySelector("body")
         element.style["background-color"] = colormap[msg]
 
         const text = document.getElementById("kleur-text")
         text.innerHTML = colornaam[msg]
+    })
+
+    connection.on("qr-code-scanned", msg => {
+        const data = JSON.parse(msg)
     })
 
     socket.addEventListener("open", e => {
